72d3128ff56c96cd97eb6613a0db5ca92acf0630
Joe Mugare <joemugare@gmail.com>
2025-08-15 15:56:44 +0300

    Update project with local changes including updated HTML template

--- crypto_tracker/settings.py
+++ crypto_tracker/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "tracker" / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
